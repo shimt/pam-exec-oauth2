5af3b4d7683494bac69ee18166d2c9f035c1e541
Shinichi MOTOKI <shinichi-motoki@overruns.org>
2017-10-15 14:43:32 +0900

    Squashed 'vendor/golang.org/x/oauth2/' changes from 30fcca6..bb50c06
    bb50c06 oauth2/internal: remove duplicate package documentation
    13449ad internal: urlencode client id and secret in header
    d89af98 oauth2: improve the custom HTTP client example
    3d1522b oauth2: add examples for basic/custom HTTP client
    9a379c6 google: add JSON field to DefaultCredentials
    96fca6c LICENSE: attribute to the Go authors
    b53b38a README: add links for contributions
    cce311a internal: fix broken auth header provider
    626d87b internal: Use provided context in subsequent request
    5432cc9 internal: add broken auth header provider
    f047394 oauth2: add test for preserving refresh token if none is received
    ad516a2 oauth2: adds sipgate api to brokenAuthHeaderProviders
    e7a4820 internal: add Shopify to list of broken auth providers
    a6bd8ce amazon: add amazon endpoints
    7fdf099 clientcredentials: update RFC doc link
    7374b3f internal: recognize Salesforce and Okta domains as broken providers
    
    git-subtree-dir: vendor/golang.org/x/oauth2
    git-subtree-split: bb50c06baba3d0c76f9d125c0719093e315b5b44

--- internal/token.go
+++ internal/token.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package internal contains support packages for oauth2 package.
 package internal
 
 import (
@@ -18,6 +17,7 @@ import (
 	"time"
 
 	"golang.org/x/net/context"
+	"golang.org/x/net/context/ctxhttp"
 )
 
 // Token represents the crendentials used to authorize
@@ -105,6 +105,7 @@ var brokenAuthHeaderProviders = []string{
 	"https://graph.facebook.com", // see https://github.com/golang/oauth2/issues/214
 	"https://login.microsoftonline.com/",
 	"https://login.salesforce.com/",
+	"https://login.windows.net",
 	"https://oauth.sandbox.trainingpeaks.com/",
 	"https://oauth.trainingpeaks.com/",
 	"https://oauth.vk.com/",
@@ -120,6 +121,15 @@ var brokenAuthHeaderProviders = []string{
 	"https://www.wunderlist.com/oauth/",
 	"https://api.patreon.com/",
 	"https://sandbox.codeswholesale.com/oauth/token",
+	"https://api.sipgate.com/v1/authorization/oauth",
+}
+
+// brokenAuthHeaderDomains lists broken providers that issue dynamic endpoints.
+var brokenAuthHeaderDomains = []string{
+	".force.com",
+	".myshopify.com",
+	".okta.com",
+	".oktapreview.com",
 }
 
 func RegisterBrokenAuthHeaderProvider(tokenURL string) {
@@ -139,6 +149,14 @@ func providerAuthHeaderWorks(tokenURL string) bool {
 		if strings.HasPrefix(tokenURL, s) {
 			// Some sites fail to implement the OAuth2 spec fully.
 			return false
+		}
+	}
+
+	if u, err := url.Parse(tokenURL); err == nil {
+		for _, s := range brokenAuthHeaderDomains {
+			if strings.HasSuffix(u.Host, s) {
+				return false
+			}
 		}
 	}
 
@@ -169,9 +187,9 @@ func RetrieveToken(ctx context.Context, clientID, clientSecret, tokenURL string,
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	if !bustedAuth {
-		req.SetBasicAuth(clientID, clientSecret)
+		req.SetBasicAuth(url.QueryEscape(clientID), url.QueryEscape(clientSecret))
 	}
-	r, err := hc.Do(req)
+	r, err := ctxhttp.Do(ctx, hc, req)
 	if err != nil {
 		return nil, err
 	}
